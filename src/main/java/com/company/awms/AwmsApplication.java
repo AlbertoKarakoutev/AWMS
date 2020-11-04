7d8f0701f2fce84b5f9f326689ad372df8534396
gbarbutov <160-gbarbutov@users.noreply.gitlab-talentboost.vmware.com>
2020-11-04 13:02:45 +0200

    Added Spring Boot project Skeleton

---
+++ src/main/java/com/company/awms/AwmsApplication.java
@@ -0,0 +1,13 @@
+package com.company.awms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/awms/AwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.awms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
