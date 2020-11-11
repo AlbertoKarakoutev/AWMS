92db7712ce6b1d077d3334561b2d7fbce2df3ecb
Berto99 <59377855+Berto99@users.noreply.github.com>
2020-11-11 13:18:49 +0200

    Document retriever by acces level
    Employee has access to all the documents in his department with their level or lower

--- src/main/java/com/company/awms/services/DocumentService.java
+++ src/main/java/com/company/awms/services/DocumentService.java
@@ -1,9 +1,12 @@
 package com.company.awms.services;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.company.awms.data.documents.DocumentRepo;
+import com.company.awms.data.documents.File;
 
 @Service
 public class DocumentService {
@@ -15,6 +18,27 @@ public class DocumentService {
         DocumentService.documentRepo = documentRepo;
     }
     
+    //Access to all documents form the same dpt. and same or lower level
+    public ArrayList<String> getAccessableDocumentIDs(String accessLevel) {
+		ArrayList<String> accessableDocumentIDs = new ArrayList<>();
+		int level = 0;
+		@SuppressWarnings("null")
+		char department = (Character) null;
+		try {
+			department = accessLevel.charAt(0);
+			level = Integer.parseInt(String.valueOf(accessLevel.charAt(1)));		
+		}catch(Exception e) {
+			System.out.println("Error retrieving access level!");
+			return null;
+		}
+		for(int i = 0; i <= level; i++) {
+			ArrayList<File> thisLevelDocuments = documentRepo.findByAccessLevel(Character.toString(department) + Integer.toString(i));
+			for(File document : thisLevelDocuments) {
+				accessableDocumentIDs.add(document.getID());
+			}
+		}
+		return accessableDocumentIDs;
+	}
     
     public static DocumentRepo getRepository() {
     	return documentRepo;
