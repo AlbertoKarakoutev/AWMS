82924c84fdfcd0802cf9bb0288c6768599bd16f9
Berto99 <59377855+Berto99@users.noreply.github.com>
2021-03-19 01:45:14 +0200

    Small bug fixes and frontend polishing

--- src/main/java/com/company/awms/modules/base/IndexController.java
+++ src/main/java/com/company/awms/modules/base/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.company.awms.modules.base.employees.EmployeeService;
 import com.company.awms.modules.base.employees.data.Employee;
+import com.company.awms.modules.base.employees.data.Notification;
 import com.company.awms.security.EmployeeDetails;
 
 @Controller
@@ -28,6 +29,7 @@ public class IndexController {
         try {
             Employee employee = employeeService.getEmployee(employeeDetails.getID());
             model.addAttribute("employee", employee);
+            model.addAttribute("credentials", Notification.getCredentials());
             return "base/employees/index";
         } catch (IOException e) {
             e.printStackTrace();
